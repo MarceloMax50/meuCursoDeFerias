c799f7474ec7dfa47fd26c6a2fd09a59cb551f57
MarceloMax50 <maxmelo93@gmail.com>
2021-07-11 18:22:50 -0300

    Annotations Controller

--- src/main/java/api/cursoDeFerias/api/PersonController.java
+++ src/main/java/api/cursoDeFerias/api/PersonController.java
@@ -2,15 +2,24 @@ package api.cursoDeFerias.api;
 
 import api.cursoDeFerias.model.Person;
 import api.cursoDeFerias.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/api/person")
+@RestController
 public class PersonController {
     private final PersonService personService;
 
+    @Autowired
     public PersonController(PersonService personService) {
         this.personService = personService;
     }
 
-    public void addPerson(Person person) {
+    @PostMapping
+    public void addPerson(@RequestBody Person person) {
         personService.addPerson(person);
     }
 }
